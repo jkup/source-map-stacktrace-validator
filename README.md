23503008eb79c6d5d7fd95dd486c09c8848d0d08
Jon Kuperman <jon.kuperman@gmail.com>
2023-11-17 16:10:39 +0000

    make tests actually tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # source-map-stacktrace-validator
+
 Validate source map stacktraces
+
+`node --test testrunners/node.js`
--- testrunners/node.js
+++ testrunners/node.js
@@ -1,7 +1,11 @@
+"use strict";
+
 const fs = require("fs");
 const path = require("path");
+const assert = require("node:assert");
+const { test } = require("node:test");
 
-var spawn = require("child_process").spawn;
+const spawn = require("child_process").spawn;
 
 // Find all the folders inside "testcases"
 const directoryPath = path.join(__dirname, "../", "testcases");
@@ -12,42 +16,54 @@ const folders = files
   .map((dirent) => dirent.name);
 
 folders.forEach((subdir) => {
-  const scriptPath = path.join(directoryPath, subdir, `${subdir}.js`);
+  test(`Validate sourcemaps for ${subdir}`, () => {
+    const scriptPath = path.join(directoryPath, subdir, `${subdir}.js`);
 
-  if (fs.existsSync(scriptPath)) {
-    var child = spawn("node", ["--enable-source-maps", scriptPath]);
+    if (fs.existsSync(scriptPath)) {
+      var child = spawn("node", ["--enable-source-maps", scriptPath]);
 
-    child.stderr.on("data", function (data) {
-      const stringifiedError = data.toString();
+      child.stderr.on("data", function (data) {
+        const stringifiedError = data.toString();
 
-      const regex = /^(Error: [^\n]+)|^([^\s].*)|at (\S+) \(([^\)]+)\)/gm;
-      let match;
-      let errorFilePath,
-        errorMessageLine,
-        stackTrace = [];
-      let foundFilePath = false;
+        const regex = /^(Error: [^\n]+)|^([^\s].*)|at (\S+) \(([^\)]+)\)/gm;
+        let match;
+        let errorFilePath,
+          errorMessageLine,
+          stackTrace = [];
+        let foundFilePath = false;
 
-      while ((match = regex.exec(stringifiedError)) !== null) {
-        if (!foundFilePath && match[2]) {
-          // Error file path
-          errorFilePath = match[2];
-          foundFilePath = true;
-        } else if (match[1]) {
-          // Error message line
-          errorMessageLine = match[1];
-        } else if (
-          match[3] &&
-          match[4] &&
-          !match[4].startsWith("node:internal")
-        ) {
-          // Stack trace, skipping 'node:internal' paths
-          stackTrace.push({ functionName: match[3], functionPath: match[4] });
+        while ((match = regex.exec(stringifiedError)) !== null) {
+          if (!foundFilePath && match[2]) {
+            // Error file path
+            errorFilePath = match[2];
+            foundFilePath = true;
+          } else if (match[1]) {
+            // Error message line
+            errorMessageLine = match[1];
+          } else if (
+            match[3] &&
+            match[4] &&
+            !match[4].startsWith("node:internal")
+          ) {
+            // Stack trace, skipping 'node:internal' paths
+            stackTrace.push({ functionName: match[3], functionPath: match[4] });
+          }
         }
-      }
 
-      console.log("Error File Path:", errorFilePath);
-      console.log("Error Message:", errorMessageLine);
-      console.log("Stack Trace:", stackTrace);
-    });
-  }
+        // Assertions
+        assert(errorFilePath, "webpack://sourcemaps-playground/src/index.js:2");
+        assert(errorMessageLine, "Error: something went wrong");
+        assert.deepEqual(stackTrace, [
+          {
+            functionName: "wat",
+            functionPath: "webpack://sourcemaps-playground/src/index.js:2:9",
+          },
+          {
+            functionName: "Object.<anonymous>",
+            functionPath: "webpack://sourcemaps-playground/src/index.js:5:1",
+          },
+        ]);
+      });
+    }
+  });
 });
